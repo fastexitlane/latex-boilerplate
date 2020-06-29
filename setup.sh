8f20dca902f6bc079d5d589ddd6d56ec747dc9c7
Dominik Becker <dominik.becker@fastexitlane.com>
2020-06-29 22:28:55 +0200

    #40 added setup script to init repo

--- setup.sh
+++ setup.sh
@@ -15,10 +15,12 @@ fi
 if [ $1 == "markdown" ]
 then
     cp templates/markdown.gitlab-ci.yml ./.gitlab-ci.yml
+    cp templates/markdown.azure-pipelines.yml ./azure-pipelines.yml
     cp templates/markdown.tasks.json ./.vscode/tasks.json
 elif [ $1 == "latex" ]
 then
     cp templates/latex.gitlab-ci.yml ./.gitlab-ci.yml
+    cp templates/latex.azure-pipelines.yml ./azure-pipelines.yml
     cp templates/latex.tasks.json ./.vscode/tasks.json
 else
     write_help
